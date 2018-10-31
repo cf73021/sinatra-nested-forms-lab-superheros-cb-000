eef9ff564c3fab94ae91c69cc0e53ca554d6082e
Chris Franklin <cf73021@gmail.com>
2018-10-31 02:16:18 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,15 @@
 class Team
+  attr_accessor :name, :motto
+
+  PIRATES = []
+   def initialize(args)
+    @name = args[:name]
+    @weight = args[:motto]
+    PIRATES << self
+  end
+   def self.all
+    PIRATES
+  end
+end
+
 end
